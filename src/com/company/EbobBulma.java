89db82f68278645ca7184605390cad33bd3b2d8d
Mustafa-Deveci <m.deveci9@hotmail.com>
2021-03-06 19:55:37 +0300

    added gcd finder

--- src/com/company/EbobBulma.java
+++ src/com/company/EbobBulma.java
@@ -7,5 +7,13 @@ public class EbobBulma {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Değer");
+        int num = s.nextInt();
+        int num2 = s.nextInt();
+        int gcd = 1;
+        for (int i = 1; i <= Math.min(num, num2); i++)
+            if (num % i == 0 && num2 % i == 0)
+                gcd = i;
+
+        System.out.println("Ebob: " + gcd);
     }
 }
