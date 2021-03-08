d2bc1d3ac74d5c9320edab415dd2345552269d50
Mustafa-Deveci <m.deveci9@hotmail.com>
2021-03-08 23:44:27 +0300

    interest account has been added.

---
+++ src/com/company/FaizHesabi.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FaizHesabi {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Para miktarı giriniz : ");
+        int miktar = s.nextInt();
+
+        System.out.print("AY cinsinden zaman giriniz : ");
+        int ayZaman = s.nextInt();
+
+        System.out.print("Faiz oranını giriniz : ");
+        double faizOrani = s.nextDouble();
+
+        double tutar = (miktar * ayZaman * faizOrani) / 100;
+
+        System.out.println("Tutar : " + tutar);
+
+
+    }
+}
