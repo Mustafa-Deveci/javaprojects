9665d84ecf938eb56c79abb5b72afae2409b21a3
Mustafa-Deveci <m.deveci9@hotmail.com>
2021-03-09 23:03:27 +0300

    odd or even number?

---
+++ src/com/company/TekCift.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class TekCift {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = s.nextInt();
+        int kalan = sayi % 2;
+
+        switch (kalan) {
+            case 0:
+                System.out.println("Girdiğini sayı çifttir.");
+                break;
+            case 1:
+                System.out.println("Girdiğiniz sayı tektir.");
+                break;
+            default:
+                System.out.println("Hata oluştu.");
+                break;
+        }
+    }
+}
